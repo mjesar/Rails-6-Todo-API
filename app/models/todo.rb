0b1d05dcda29a080e055df6e93237c6a2f336bd2
mjesar <mohammadalijaisar@gmail.com>
2019-11-18 15:11:38 +0500

    validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :status, inclusion: { in: [true, false] }
 end
