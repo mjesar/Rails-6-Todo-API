fa5a33d042398a500a88f5127e9e2c117116f968
mjesar <mohammadalijaisar@gmail.com>
2019-11-25 22:39:49 +0500

    show method

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::TodosController < ApiController
+  before_action :set_todo, only: %i[show]
+
   def index
     @todos = current_user.todos.all.order(created_at: :desc).page(params[:page])
+  end
+
+  def show
+    render :show
   end
 
   private
