af639ddab04f2b332585a8c92e660d0b162f24e0
mjesar <mohammadalijaisar@gmail.com>
2019-11-25 22:37:48 +0500

    set item method

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::TodosController < ApiController
   def index
     @todos = current_user.todos.all.order(created_at: :desc).page(params[:page])
   end
+
+  private
+
+  def set_todo
+    @todo = current_user.todos.find(params[:id])
+  end
 end
