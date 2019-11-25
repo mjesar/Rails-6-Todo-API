d8c5aeb0e48d5aa4a9fa13746781610613ebb93d
mjesar <mohammadalijaisar@gmail.com>
2019-11-25 22:35:24 +0500

    api pagination info

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TodosController < ApiController
   def index
-    @todos = current_user.todos.all
+    @todos = current_user.todos.all.order(created_at: :desc).page(params[:page])
   end
 end
--- app/views/api/v1/todos/index.json.jbuilder
+++ app/views/api/v1/todos/index.json.jbuilder
@@ -1,3 +1,7 @@
+json.current @todos.current_page
+json.per_page @todos.limit_value
+json.pages @todos.total_pages
+json.count @todos.total_count
 json.sys do
   json.type 'Array'
 end
