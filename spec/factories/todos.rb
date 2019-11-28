a884a9f026ef493b194b4ee05c254a19d50e327c
mjesar <mohammadalijaisar@gmail.com>
2019-11-28 21:21:23 +0500

    fix long line issue with faker 1 sentance

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :todo do
     title { Faker::FunnyName.name }
-    description { Faker::Lorem.paragraph(sentence_count: 2) }
+    description { Faker::Lorem.paragraph(sentence_count: 1) }
     status { false }
   end
 end
