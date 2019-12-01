286138795c2a2356446ebdb6b05af48746f18b67
mjesar <mohammadalijaisar@gmail.com>
2019-12-01 16:33:59 +0500

    signout controller remove sessions and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/signout', to: 'user/sessions#destroy', as: :signout
+  end
   root to: 'home#index'
   namespace :api do
     namespace :v1 do
